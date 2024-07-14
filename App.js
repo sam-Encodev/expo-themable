aed1c8b0b9eac16b941fe6413569479e5a2b57d7
Sam <samuel@encodevlabs.com>
2024-07-14 21:19:27 +0000

    component cleanup

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import Main from "./Main";
-import FAB from "./components/FAB";
+import Main from "./src/Main";
 import { useFonts } from "expo-font";
+import FAB from "./src/components/FAB";
 import { TamaguiProvider } from "tamagui";
 import { useEffect, useState } from "react";
-import { useColorScheme } from "react-native";
-import { tamaguiConfig } from "./utils/constants";
 import { PaperProvider } from "react-native-paper";
+import { useColorScheme, View } from "react-native";
+import { tamaguiConfig } from "./src/utils/constants";
 import Ionicons from "react-native-vector-icons/Ionicons";
-
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
  const colorScheme = useColorScheme();
@@ -29,15 +29,17 @@ export default function App() {
  }
 
  return (
-  <TamaguiProvider config={tamaguiConfig} defaultTheme={theme}>
-   <PaperProvider
-    settings={{
-     icon: (props) => <Ionicons {...props} />,
-    }}
-   >
-    <Main />
-    <FAB theme={theme} setTheme={setTheme} />
-   </PaperProvider>
-  </TamaguiProvider>
+  <SafeAreaProvider>
+   <TamaguiProvider config={tamaguiConfig} defaultTheme={theme}>
+    <PaperProvider
+     settings={{
+      icon: (props) => <Ionicons {...props} />,
+     }}
+    >
+     <Main />
+     <FAB theme={theme} setTheme={setTheme} />
+    </PaperProvider>
+   </TamaguiProvider>
+  </SafeAreaProvider>
  );
 }
