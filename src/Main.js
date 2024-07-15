841cecc9841a153f388e7b41169cade858800967
Sam <samuel@encodevlabs.com>
2024-07-15 11:12:46 +0000

    refactor: Remove unused imports and clean up Main component

--- src/Main.js
+++ src/Main.js
@@ -1,24 +1,10 @@
-import { View, useTheme } from "tamagui";
 import { Curved } from "./components/Curved";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function Main() {
- const activeTheme = useTheme();
- const insets = useSafeAreaInsets();
-
  return (
-  <View
-   //    height="100%"
-   paddingTop={insets.top}
-   paddingLeft={insets.left}
-   paddingRight={insets.right}
-   //    paddingBottom={insets.bottom}
-   backgroundColor={activeTheme.bg}
-  >
-   <Curved>
-    <Curved.Header />
-    <Curved.View />
-   </Curved>
-  </View>
+  <Curved>
+   <Curved.Header />
+   <Curved.View />
+  </Curved>
  );
 }
